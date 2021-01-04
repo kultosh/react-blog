8bdfda7b215592b1c9dbf152d5eb54a367a396a3
kultosh <kulgole90@gmail.com>
2021-01-04 21:48:20 +0545

    Created array mapping for outputting the list of an object

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,19 @@
 import {useState} from 'react';
 function Home() {
-    const [name, setname] = useState('Prasidha');
-    const [age, setage] = useState(9);
-    const handleClick  = () => {
-       setname('Santosh');
-       setage(26);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'Learn React', body: 'lorem ipsum ...', author: 'Roger', id: 1},
+        { title: 'Learn Laravel', body: 'lorem ipsum ...', author: 'Nadal', id: 2},
+        { title: 'Learn Vue', body: 'lorem ipsum ...', author: 'Andy', id: 3}
+    ]);
 
     return (
         <div className="home">
-            <h2>HomePage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <i>Written By: {blog.author}</i>
+                </div>
+            ))}
         </div>
     )
 }
