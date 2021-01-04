8c0ae6e9fdbc910864bcc1249cd5e2e0968814c7
kultosh <kulgole90@gmail.com>
2021-01-04 18:05:56 +0545

    Created click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 function Home() {
+    const handleClick  = () => {
+        console.log('hello')
+    }
+
+    const handleClickAgain  = (name) => {
+        console.log('New Hello ' + name);
+    }
+
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={() => handleClickAgain('Santosh')}>Click Me</button>
         </div>
     )
 }
