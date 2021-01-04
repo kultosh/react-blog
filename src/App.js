4877af2c04ab1c024d595036fbc47d0486c01a2d
kultosh <kulgole90@gmail.com>
2021-01-04 15:13:27 +0545

    Created dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to new blog!';
+  const link = "https://github.com/kultosh";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h2>React Blog</h2>
+        <p>{title}</p>
+        <a href={link}>My Github</a>
+      </div>
     </div>
   );
 }
