98001a5ddb390fdf688a6a814fb28cd999805502
kultosh <kulgole90@gmail.com>
2021-01-05 20:37:24 +0545

    Created filter reusing same props and components

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,13 @@ function Home() {
     const [blogs, setBlogs] = useState([
         { title: 'Learn React', body: 'lorem ipsum ...', author: 'Roger', id: 1},
         { title: 'Learn Laravel', body: 'lorem ipsum ...', author: 'Nadal', id: 2},
-        { title: 'Learn Vue', body: 'lorem ipsum ...', author: 'Andy', id: 3}
+        { title: 'Learn Vue', body: 'lorem ipsum ...', author: 'Roger', id: 3}
     ]);
 
     return (
         <div className="home">
             <BlockList blogs={blogs} title="All Blogs!" />
+            <BlockList blogs={blogs.filter((blog)=>blog.author === 'Roger')} title="Roger's Blogs" />
         </div>
     )
 }
