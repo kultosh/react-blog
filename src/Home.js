2cffec45da20c3e37e992d7996a4dcd2ead010a8
kultosh <kulgole90@gmail.com>
2021-01-12 13:33:05 +0545

    Created conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,26 @@
 import {useState, useEffect} from 'react';
 import BlockList from './BlogList';
 function Home() {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        // Note: dont put the setTimeout function in the live server because it gonna make user wait for 1 second more. Here it is just used to show its uses
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
         .then(res => {
             return res.json();
         })
         .then(data => {
             setBlogs(data);
-        })
+        });
+        setIsLoading(false);
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {isLoading && <div>Loading....</div> }
             {blogs && <BlockList blogs={blogs} title="All Blogs!" />}
         </div>
     )
