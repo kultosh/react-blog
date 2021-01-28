72aac1775ec2a8d59e8e8cec5fa30ad5b6cd19cd
kultosh <kulgole90@gmail.com>
2021-01-28 15:56:22 +0545

    Created route parameters to get detail of the blogs through an id

---
+++ src/BlogDetails.js
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
+
+const BlogDetails = () => {
+    const { id } = useParams();
+    const {data: blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
+    console.log(blog);
+    return ( 
+        <div className="blog-details">
+            { isLoading && <div> Loading... </div>}
+            { error && <div>{error}</div>}
+            { blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written By: {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            ) }
+        </div>
+     );
+}
+ 
+export default BlogDetails;
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 function BlockList ({blogs, title}) {
     return (
         <div className="block-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <i>Written By: {blog.author}</i>
+                    <Link to={`blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <i>Written By: {blog.author}</i>
+                    </Link>    
                 </div>
             ))}
         </div>
