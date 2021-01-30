d6403aadf0a8ba59be77ef31767ebc015f52823d
kultosh <kulgole90@gmail.com>
2021-01-30 22:37:28 +0545

    Created delete request to delete the blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
-    console.log(blog);
+    const history = useHistory();
+
+    const handleBlog = (e) => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE',
+        }).then(() => {
+            history.push('/');
+        })
+    }
+    
     return ( 
         <div className="blog-details">
             { isLoading && <div> Loading... </div>}
@@ -14,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written By: {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleBlog}>Delete</button>
                 </article>
             ) }
         </div>
